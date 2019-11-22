67a1bff007833f29f5030b275ac79dc5b5f88a7c
rileyhales <rileyhales1@gmail.com>
2019-11-22 12:16:59 -0700

    fix time dependency

--- data_workflow/onion_creek_events/workflow.sh
+++ data_workflow/onion_creek_events/workflow.sh
@@ -12,22 +12,22 @@ fi
 echo "Downloading Data..."
 
 cd
-touch .netrc
-echo "machine urs.earthdata.nasa.gov login tethysgldas password KKP4E2sjTfQGsMX" >> .netrc
-touch .urs_cookies
+#touch .netrc
+#echo "machine urs.earthdata.nasa.gov login tethysgldas password KKP4E2sjTfQGsMX" >> .netrc
+#touch .urs_cookies
 
 chmod -R 0755 $1
-mkdir -p $1/2013event/
-cd $1/2013event/
-cat $DIR/2013event_urls.txt | tr -d '\r' | xargs -n 1 -P 4 curl -LJO -n -c ~/.urs_cookies -b ~/.urs_cookies
+#mkdir -p $1/2013event/
+#cd $1/2013event/
+#cat $DIR/2013event_urls.txt | tr -d '\r' | xargs -n 1 -P 4 curl -LJO -n -c ~/.urs_cookies -b ~/.urs_cookies
 
 mkdir -p $1/2015event/
 cd $1/2015event/
 cat $DIR/2015event_urls.txt | tr -d '\r' | xargs -n 1 -P 4 curl -LJO -n -c ~/.urs_cookies -b ~/.urs_cookies
 
-mkdir -p $1/2018event/
-cd $1/2018event/
-cat $DIR/2018event_urls.txt | tr -d '\r' | xargs -n 1 -P 4 curl -LJO -n -c ~/.urs_cookies -b ~/.urs_cookies
+#mkdir -p $1/2018event/
+#cd $1/2018event/
+#cat $DIR/2018event_urls.txt | tr -d '\r' | xargs -n 1 -P 4 curl -LJO -n -c ~/.urs_cookies -b ~/.urs_cookies
 
 echo "Download Done"
 
